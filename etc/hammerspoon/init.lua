fa76db14f15933ca07a501955bee043c157453c6
Burke Libbey <burke@libbey.me>
2021-03-16 19:25:55 -0400

    oh

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -48,7 +48,7 @@ appbindings.setup({"cmd", "ctrl"}, {
   [3] = "com.brave.Browser",
   [4] = "com.tinyspeck.slackmacgap",
   [5] = nil,
-  [6] = "com.apple.Messages",
+  [6] = "com.apple.MobileSMS",
   [7] = "org.whispersystems.signal-desktop",
   [8] = nil,
   [9] = nil,
